62f009d4278e28aaf989206d3f2dede021d04640
Mikolaj Lewandowski <mikulew@gmail.com>
2021-05-29 23:27:30 +0200

    Add type module attribute and export OPERATION to consts

---
+++ src/consts/index.js
@@ -0,0 +1,9 @@
+export const OPERATION = {
+  ADD: 'add',
+  SUBSTRACT: 'subtract',
+  MULTIPLY: 'multiply',
+  DIVIDE: 'divide',
+  DOT: 'dot',
+  CLEAR: 'clear',
+  EQUAL: 'equal'
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-const OPERATION = {
-  ADD: 'add',
-  SUBSTRACT: 'subtract',
-  MULTIPLY: 'multiply',
-  DIVIDE: 'divide',
-  DOT: 'dot',
-  CLEAR: 'clear',
-  EQUAL: 'equal'
-};
+import { OPERATION } from './consts/index.js';
 
 let numbers = [];
 let operations = [];
